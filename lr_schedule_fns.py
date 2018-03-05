d76c5efb00f72481072a2c126543361fe8d90960
Rishabh Jain <rj2315@imperial.ac.uk>
2018-03-05 19:34:08 +0000

    Added learning rate schedule functions.

---
+++ lr_schedule_fns.py
@@ -0,0 +1,18 @@
+learning_rate = 0.009
+lr_param = 0.97
+def decay_after_constant(epochs):
+    denominator = max(epochs, lr_param)
+    return float(learning_rate) * lr_param / denominator
+
+def decay_scaling_factor(epochs):
+    return float(learning_rate) * (lr_param ** epochs)
+
+def decay_constant(epochs):
+    denominator = 1 + (epochs / lr_param)
+    return float(learning_rate) / denominator
+
+def get_learning_rate():
+    return learning_rate
+
+def get_lr_param():
+    return lr_param
