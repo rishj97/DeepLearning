e0e254e5624e9a2d95896cd6bbfd93c04d309481
Rishabh Jain <rj2315@imperial.ac.uk>
2018-03-06 16:12:47 +0000

    Bug fixes in the graph gen script.

--- plot_graph.py
+++ plot_graph.py
@@ -1,9 +1,12 @@
-import matplotlib as mpl
+import matplotlib
+matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import datetime
-
+from scipy.interpolate import interp1d, spline
+from scipy import interpolate
+from scipy.signal import savgol_filter
 # Arguments: 1: Plot title
 #            2: Y axis label
 #            3: 1st csv file
@@ -18,11 +21,17 @@ plt.xlabel('Epoch number')
 plt.ylabel(str(sys.argv[2]))
 colors = ['r', 'b', 'g', 'c', 'm', 'y']
 i = 3
+c = 0
+file_name = ''
 while(i < len(sys.argv)):
-    print(i)
+    file_name += str(sys.argv[i])
     data = np.genfromtxt(sys.argv[i], delimiter=',', names=True)
-    plt.plot(data['Step'], data['Value'], c=colors[i-3], label=str(sys.argv[i + 1]))
+    x = np.array(data['Step'])
+    y = np.array(data['Value'])
+    y_smooth = savgol_filter(y, 7, 2)
+    plt.plot(x, y_smooth, c=colors[c], label=str(sys.argv[i + 1]))
+    c += 1
     i += 2
 
 plt.legend()
-plt.savefig(str(sys.argv[1]) + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")) + '.png')
+plt.savefig(str(sys.argv[1]) + '.png')
